dc0efea348bc8eacc5a4f251a4db803416aa0229
Anton Berkutov <void__fastcall@mail.ru>
2022-07-07 01:43:22 +0300

    Initial commit

---
+++ app/javascript/scripts/custom.js
@@ -0,0 +1,7 @@
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// })
+
+// document.querySelector(function () {
+//   document.querySelector('[data-toggle="popover"]').popover()
+// })
