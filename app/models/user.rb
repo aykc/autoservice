5b2805b5a157d794100fad2bbdcad396782340f0
Anton Berkutov <void__fastcall@mail.ru>
2022-07-16 00:38:19 +0300

    refactor user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,6 @@
 class User < ApplicationRecord
-  has_many :line_items
+  has_many :line_items, dependent: :restrict_with_error
   has_many :services, through: :line_items
 
-  before_destroy :check_deletion
   validates :name, presence: true
-
-  private
-  def check_deletion
-    if line_items.any?
-      errors.add(:user, 'Нельзя удалить исполнителя, если он присутствует в нарядах') if line_items.any?
-      throw :abort
-    end
-    return true
-  end
 end
